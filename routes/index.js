2501dda30cc0a3484dcd42404b71fba3de35e4a0
Akshat Agrawal <akshatagrawal@Akshats-MacBook-Pro.local>
2020-10-07 22:06:58 -0700

    again

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ var router = express.Router();
 const pg = require('pg');
 const connectionstring = "postgres://jymmcobmzetihf:6653e9e126dfda00ce77a5d4e59a6fae40e37604d46ec703aabed728b8bcba41@ec2-54-166-251-173.compute-1.amazonaws.com:5432/d2ot4v4ca446jk"
 
+const client = new pg.Client(process.env.DATABASE_URL);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/names', function(req, res, next) {
-  console.log(process.env.DATABASE_URL);
-  const client = new pg.Client(process.env.DATABASE_URL, (err, res) => {
-    console.log("connected!")
-  });
+  client.connect();
+
   // await client.connect(process.env.DATABASE_URL);
   // res = await client.query('SELECT first_name FROM sample_pii');
   // res.send(res);
