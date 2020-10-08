a09e310fcaa8dbaa7eb9c2f1323416a01b467964
Akshat Agrawal <akshatagrawal@Akshats-MacBook-Pro.local>
2020-10-08 08:52:11 -0700

    connect to db

---
+++ config.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const pg = require('pg');
+const isProd = process.env.NODE_ENV === 'production'
+
+const connectionString = "postgres://jymmcobmzetihf:6653e9e126dfda00ce77a5d4e59a6fae40e37604d46ec703aabed728b8bcba41@ec2-54-166-251-173.compute-1.amazonaws.com:5432/d2ot4v4ca446jk"
+
+const client = new pg({
+    connectionString: isProd ? process.env.DATABASE_URL : connectionString, 
+    ssl: isProd
+})
+
+module.exports = client
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-const pg = require('pg');
-const connectionstring = "postgres://jymmcobmzetihf:6653e9e126dfda00ce77a5d4e59a6fae40e37604d46ec703aabed728b8bcba41@ec2-54-166-251-173.compute-1.amazonaws.com:5432/d2ot4v4ca446jk"
-
-const client = new pg.Client(process.env.DATABASE_URL);
+const db = require('../config')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +8,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/names', function(req, res, next) {
-  client.connect();
+  db.query('SELECT * FROM sample_pii', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.json(results.rows)
+  })
 
   // await client.connect(process.env.DATABASE_URL);
   // res = await client.query('SELECT first_name FROM sample_pii');
