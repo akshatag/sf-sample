2654bbb0a0facbbb1a1fa65bd1ee9df8d50082d4
Akshat Agrawal <akshatagrawal@Akshats-MacBook-Pro.local>
2020-10-08 21:35:59 -0700

    working on local

--- config.js
+++ config.js
@@ -1,14 +1,19 @@
 require('dotenv').config()
 
-const pg = require('pg');
+const Pool = require('pg').Pool;
+
 const isProd = process.env.NODE_ENV === 'production'
 
 const connectionString = "postgres://jymmcobmzetihf:6653e9e126dfda00ce77a5d4e59a6fae40e37604d46ec703aabed728b8bcba41@ec2-54-166-251-173.compute-1.amazonaws.com:5432/d2ot4v4ca446jk"
 
-const client = new pg.Client({
+console.log('connecting to db...')
+
+const pool = new Pool({
     connectionString: isProd ? process.env.DATABASE_URL : connectionString, 
-    ssl: isProd
+    ssl: {rejectUnauthorized: false}
 })
 
-module.exports = client
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+}
 
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const db = require('../config')
+const db = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/names', function(req, res, next) {
-  db.query('SELECT * FROM sample_pii', (error, results) => {
-    if (error) {
-      throw error;
-    }
-    res.json(results.rows)
-  })
-
-  // await client.connect(process.env.DATABASE_URL);
-  // res = await client.query('SELECT first_name FROM sample_pii');
-  // res.send(res);
-  // await client.end();
+router.get('/all', async function(req, res, next) {
+  results = await db.query('SELECT * FROM sample_pii')
+  res.send(results.rows)
 })
 
 module.exports = router;
